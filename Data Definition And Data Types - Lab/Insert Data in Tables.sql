8916dde2b2d9b99d2d43574cad6e0b3d0cd50fcb
Valentin Tsonevski <tsonevski99@gmail.com>
2022-11-05 16:21:32 +0000

    Add files via upload

---
+++ Data Definition And Data Types - Lab/Insert Data in Tables.sql
@@ -0,0 +1,10 @@
+INSERT INTO employees (first_name, lsat_name)
+VALUES ('Pesho', 'Petrov');
+
+INSERT INTO employees (first_name, last_name)
+VALUES ('Georgi','Georgiev');
+
+INSERT INTO employees (first_name, last_name)
+VALUES ('Georgi22','Georgiev22');
+
+
