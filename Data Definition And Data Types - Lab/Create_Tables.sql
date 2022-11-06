af1e263eca5f2fbe818a50ed8501b1dff43d83b6
Valentin Tsonevski <tsonevski99@gmail.com>
2022-11-06 10:47:39 +0000

    Add files via upload

---
+++ Data Definition And Data Types - Lab/Create_Tables.sql
@@ -0,0 +1,18 @@
+CREATE TABLE `employees`(
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+`first_name` VARCHAR(20) NOT NULL,
+`last_name` VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE `categories`(
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(20) NOT NULL
+);
+
+
+CREATE TABLE `products`(
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(20) NOT NULL,
+`category_id` INT NOT NULL
+);
+
