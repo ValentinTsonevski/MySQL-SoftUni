4f5b1856313d5743d7812b762febc61962fed11d
Valentin Tsonevski <tsonevski99@gmail.com>
2023-01-03 18:24:06 +0000

    Delete Employee Departments.sql

--- Employee Departments.sql
+++
@@ -1,7 +0,0 @@
-SELECT e.`employee_id`,e.`first_name`,e.`salary`,d.`name` AS `department_name`
-FROM `employees` AS e
-JOIN `departments` AS d
-ON e.`department_id` = d.`department_id`
-WHERE `salary` > 15000
-ORDER BY d.`department_id` DESC
-LIMIT 5;
